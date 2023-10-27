c398e2273b1b672ef94ea5f1ae1e3c430592d446
joswa123 <joswa148@gmail.com>
2023-10-27 16:16:31 +0530

    modified index.jsx

--- dist/index.js
+++ dist/index.js
@@ -8,10 +8,130 @@ const {
   iqCallee
 } = require("@xmpp/component");
 const debug = require("@xmpp/debug");
+const domain = "deepak.hool.org";
 const sender = component({
   service: "xmpp://beta.hool.org",
-  domain: "deepak.hool.org",
+  domain: domain,
   password: "deepak@123!"
 });
 sender.start();
+sender.iqCallee.get("http://jabber.org/protocol/disco#items", "query", async ctx => {
+  const fromJID = ctx.stanza.attrs.to;
+  const firstname = fromJID.split("@")[0];
+  if (fromJID == domain) {
+    const games = ["kida", "carddoo", "faeko", "roop", "tricko"];
+    return xml("query", {
+      xmlns: "http://jabber.org/protocol/disco#items"
+    }, xml("feature", {
+      var: "http://jabber.org/protocol/disco#info"
+    }), xml("feature", {
+      var: "http://jabber.org/protocol/disco#items"
+    }), xml("item", {
+      jid: "kida@deepak.hool.org",
+      name: "kida"
+    }), xml("item", {
+      jid: "cardoo@deepak.hool.org",
+      name: "cardoo"
+    }), xml("item", {
+      jid: "faeko@deepak.hool.org",
+      name: "faeko"
+    }), xml("item", {
+      jid: "roop@deepak.hool.org",
+      name: "roop"
+    }), xml("item", {
+      jid: "tricko@deepak.hool.org",
+      name: "tricko"
+    }), xml("item", {
+      jid: "hool@deepak/.hool.org",
+      name: "hool"
+    }));
+  }
+  if (firstname === `hool`) {
+    const game = {
+      name: "hool",
+      jid: fromJID,
+      maximumPlayers: "4",
+      GameType: "board",
+      minimumplayer: "4",
+      app_available: "yes"
+    };
+    return xml("query", {
+      xmlns: "http://jabber.org/protocol/disco#items"
+    }, xml("feature", {
+      var: "http://jabber.org/protocol/disco#info"
+    }), xml("feature", {
+      var: "http://jabber.org/protocol/disco#items"
+    }), xml("name", null, game.name), xml("maximumPlayers", null, game.maximumPlayers), xml("gameType", null, game.GameType), xml("minPlayer", null, game.minimumplayer), xml("jid", null, game.jid));
+  }
+  if (firstname === `faeko`) {
+    const game = {
+      name: "faeko",
+      maximumPlayers: "6",
+      GameType: "card`",
+      minimumplayer: "2",
+      jid: fromJID,
+      app_available: "yes"
+    };
+    return xml("query", {
+      xmlns: "http://jabber.org/protocol/disco#items"
+    }, xml("feature", {
+      var: "http://jabber.org/protocol/disco#info"
+    }), xml("feature", {
+      var: "http://jabber.org/protocol/disco#items"
+    }), xml("name", null, game.name), xml("jid", null, game.jid), xml("maximumPlayers", null, game.maximumPlayers), xml("gameType", null, game.GameType), xml("minPlayer", null, game.minimumplayer), xml("app_available", null, game.app_available));
+  }
+  if (firstname === `kida`) {
+    const game = {
+      name: "kida",
+      maximumPlayers: "1",
+      GameType: "card`",
+      minimumplayer: "1",
+      jid: fromJID,
+      app_available: "yes"
+    };
+    return xml("query", {
+      xmlns: "http://jabber.org/protocol/disco#items"
+    }, xml("feature", {
+      var: "http://jabber.org/protocol/disco#info"
+    }), xml("feature", {
+      var: "http://jabber.org/protocol/disco#items"
+    }), xml("name", null, game.name), xml("jid", null, game.jid), xml("maximumPlayers", null, game.maximumPlayers), xml("gameType", null, game.GameType), xml("minPlayer", null, game.minimumplayer), xml("app_available", null, game.app_available));
+  }
+  if (firstname === `roop`) {
+    const game = {
+      name: "roop",
+      maximumPlayers: "2",
+      GameType: "card`",
+      minimumplayer: "2",
+      jid: fromJID,
+      app_available: "no"
+    };
+    return xml("query", {
+      xmlns: "http://jabber.org/protocol/disco#items"
+    }, xml("feature", {
+      var: "http://jabber.org/protocol/disco#info"
+    }), xml("feature", {
+      var: "http://jabber.org/protocol/disco#items"
+    }), xml("name", null, game.name), xml("jid", null, game.jid), xml("maximumPlayers", null, game.maximumPlayers), xml("gameType", null, game.GameType), xml("minPlayer", null, game.minimumplayer), xml("app_available", null, game.app_available));
+  }
+});
+sender.iqCallee.get("http://jabber.org/protocol/disco#info", "query", async ctx => {
+  const to = ctx.stanza.attrs.to;
+  console.log(to, "joswa");
+  if (to == domain) {
+    const list = {
+      owner: "amaresh deshpande",
+      tittle: "planetHool",
+      no_of_palyers: "4",
+      game_start_time: "5min"
+    };
+    return xml("query", {
+      xmlns: "http://jabber.org/protocol/disco#info"
+    }, xml("feature", {
+      var: "http://jabber.org/protocol/disco#info"
+    }), xml("feature", {
+      var: "http://jabber.org/protocol/disco#items"
+    }), xml("owner", null, list.owner), xml("title", null, list.tittle), xml("no_of_palyers", null, list.no_of_palyers), xml("game_start_time", null, list.game_start_time));
+  }
+});
 debug(sender, true);
--- src/index.jsx
+++ src/index.jsx
@@ -2,110 +2,151 @@ const { loadPlugin } = require("@babel/core/lib/config/files");
 const { component, xml, jid, iqCallee } = require("@xmpp/component");
 const debug = require("@xmpp/debug");
 
+const domain = "deepak.hool.org";
 const sender = component({
   service: "xmpp://beta.hool.org",
-  domain: "deepak.hool.org",
+  domain: domain,
   password: "deepak@123!",
 });
+sender.start;
 
-sender.start();
+sender.iqCallee.get(
+  "http://jabber.org/protocol/disco#items",
+  "query",
+  async (ctx) => {
+    const fromJID = ctx.stanza.attrs.to;
+    const firstname = fromJID.split("@")[0];
 
-// sender.on("stanza", (stanza) => {
-//   if (stanza.is("message")) {
-//     const fromJID = stanza.attrs.from;
+    if (fromJID == domain) {
+      const games = ["kida", "carddoo", "faeko", "roop", "tricko"];
 
-//     const messageBody = stanza.getChildText("body");
-//     const fromJIDParts = fromJID.split(" ");
-//     const firstWord = fromJIDParts[0];
-   
-//     if (messageBody) {
-//       const responseMessage = ["kida ", "cardoo", "fakeo", "tricko"];
-     
-     
-//       const responseStanza = (
-//         <message type="chat" to={fromJID}>
-//           <body>{responseMessage}</body>
-          
-//         </message>
-//       );
+      return (
+        <query xmlns="http://jabber.org/protocol/disco#items">
+          <feature var="http://jabber.org/protocol/disco#info" />
+          <feature var="http://jabber.org/protocol/disco#items" />
+          <item jid="kida@deepak.hool.org" name="kida" />
+          <item jid="cardoo@deepak.hool.org" name="cardoo" />
+          <item jid="faeko@deepak.hool.org" name="faeko" />
+          <item jid="roop@deepak.hool.org" name="roop" />
+          <item jid="tricko@deepak.hool.org" name="tricko" />
+          <item jid="hool@deepak/.hool.org" name="hool" />
+        </query>
+      );
+    }
 
-      
-//       sender.send(responseStanza);
-//     }
-//     if (firstWord === "red") {
-//       const game = {
-//         maximumPlayers: "4",
-//         GameType: "board",
-//         minimumplayer: "2",
-//         jid: fromJID,
-//       };
-//       const responseTwo = (
-//         <message type="chat" to={fromJID}>
-//           <body>{game}</body>
-//         </message>
-//       );
-
-//       sender.send(responseTwo);
-//     }
-//   }
-// });
-
-// sender.on("error", (err) => {
-//   console.error(err);
-// });
-
-// const games = ["hool", "fakeo", "roop", "tricko", "hool"];
-
-// sender.iqCallee.get(
-//   "http://jabber.org/protocol/disco#info",
-//   "query",
-//   // async (ctx) => {
-//   //   console.log("q clled", ctx);
-//   // },
-
-//   async(ctx)=>{
-//     if(ctx.stanza.attrs.to === sender.domain)
-//     return(
-//   <div>
-//   {games}
-//   console.log(games);
-
-//   </div>
-//   )
-
-//   }
-// )
-
-// sender.iqCallee.set("http://jabber.org/protocol/mug#owner",
-//         "query",
-//         (ctx) => {
-//           console.log(ctx)
-//         })
-
-// sender.on("stanza", async (stanza) => {
-//   console.log('joswa',stanza);
-//   if (stanza.is("iq")) {
-//     let from = stanza.attrs.from;
-//     let to = stanza.attrs.to;
-//     console.log('HII JOSWA');
-//     await sender.send(
-
-//       <message type="chat"from={to} to={from}>
-//         <body>kida</body>
-//       </message>
-//     );
-//   }
-// });
-
-// sender.on("online", async (address) => {
-//   // console.log("online as ", address.toString());
-
-//   await sender.send((
-//     <message type="chat" to={address}>
-//       <body>hello hool</body>
-//     </message>
-//   ));
-
-// });
-
+    if (firstname === `hool`) {
+      const game = {
+        name: "hool",
+        jid: fromJID,
+        maximumPlayers: "4",
+        GameType: "board",
+        minimumplayer: "4",
+        app_available: "yes",
+      };
+      return (
+        <query xmlns="http://jabber.org/protocol/disco#items">
+          <feature var="http://jabber.org/protocol/disco#info" />
+          <feature var="http://jabber.org/protocol/disco#items" />
+          <name>{game.name}</name>
+          <maximumPlayers>{game.maximumPlayers}</maximumPlayers>
+          <gameType>{game.GameType}</gameType>
+          <minPlayer>{game.minimumplayer}</minPlayer>
+          <jid>{game.jid}</jid>
+        </query>
+      );
+    }
+    if (firstname === `faeko`) {
+      const game = {
+        name: "faeko",
+        maximumPlayers: "6",
+        GameType: "card`",
+        minimumplayer: "2",
+        jid: fromJID,
+        app_available: "yes",
+      };
+      return (
+        <query xmlns="http://jabber.org/protocol/disco#items">
+          <feature var="http://jabber.org/protocol/disco#info" />
+          <feature var="http://jabber.org/protocol/disco#items" />
+          <name>{game.name}</name>
+          <jid>{game.jid}</jid>
+          <maximumPlayers>{game.maximumPlayers}</maximumPlayers>
+          <gameType>{game.GameType}</gameType>
+          <minPlayer>{game.minimumplayer}</minPlayer>
+          <app_available>{game.app_available}</app_available>
+        </query>
+      );
+    }
+    if (firstname === `kida`) {
+      const game = {
+        name: "kida",
+        maximumPlayers: "1",
+        GameType: "card`",
+        minimumplayer: "1",
+        jid: fromJID,
+        app_available: "yes",
+      };
+      return (
+        <query xmlns="http://jabber.org/protocol/disco#items">
+          <feature var="http://jabber.org/protocol/disco#info" />
+          <feature var="http://jabber.org/protocol/disco#items" />
+          <name>{game.name}</name>
+          <jid>{game.jid}</jid>
+          <maximumPlayers>{game.maximumPlayers}</maximumPlayers>
+          <gameType>{game.GameType}</gameType>
+          <minPlayer>{game.minimumplayer}</minPlayer>
+          <app_available>{game.app_available}</app_available>
+        </query>
+      );
+    }
+    if (firstname === `roop`) {
+      const game = {
+        name: "roop",
+        maximumPlayers: "2",
+        GameType: "card`",
+        minimumplayer: "2",
+        jid: fromJID,
+        app_available: "no",
+      };
+      return (
+        <query xmlns="http://jabber.org/protocol/disco#items">
+          <feature var="http://jabber.org/protocol/disco#info" />
+          <feature var="http://jabber.org/protocol/disco#items" />
+          <name>{game.name}</name>
+          <jid>{game.jid}</jid>
+          <maximumPlayers>{game.maximumPlayers}</maximumPlayers>
+          <gameType>{game.GameType}</gameType>
+          <minPlayer>{game.minimumplayer}</minPlayer>
+          <app_available>{game.app_available}</app_available>
+        </query>
+      );
+    }
+  }
+);
+sender.iqCallee.get(
+  "http://jabber.org/protocol/disco#info",
+  "query",
+  async (ctx) => {
+    const to = ctx.stanza.attrs.to;
+    console.log(to, "joswa");
+    if (to == domain) {
+      const list = {
+        owner: "amaresh deshpande",
+        tittle: "planetHool",
+        no_of_palyers: "4",
+        game_start_time: "5min",
+      };
+      return (
+        <query xmlns="http://jabber.org/protocol/disco#info">
+          <feature var="http://jabber.org/protocol/disco#info" />
+          <feature var="http://jabber.org/protocol/disco#items" />
+          <owner>{list.owner}</owner>
+          <title>{list.tittle}</title>
+          <no_of_palyers>{list.no_of_palyers}</no_of_palyers>
+          <game_start_time>{list.game_start_time}</game_start_time>
+        </query>
+      );
+    }
+  }
+);
 debug(sender, true);
